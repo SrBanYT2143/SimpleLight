e1bd7d18e3393f8400cd16393c4beded6d73ff94
Jonas <76711582+SrBanYT2143@users.noreply.github.com>
2022-01-17 12:14:15 -0300

    Add files via upload

---
+++ SimpleLight_v1.0.0/src/BanzinhoDev/Light/Main.php
@@ -0,0 +1,55 @@
+<?php
+
+# ∆ Iniciando projeto √
+
+namespace BanzinhoDev\Light;
+
+
+# ∆ especificando as uses base do plugin
+
+use pocketmine\plugin\PluginBase;
+
+# ∆ especificando as uses de comandos
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\command\PluginCommand;
+
+# ∆ especificando as uses aleatórias a adicionar a o decorrer do plugin
+
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\Effect;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\entity\effect\EffectManager;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+use const INT32_MAX;
+use pocketmine\world\Position;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() : void{
+        
+        }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+        switch($cmd->getName()) {
+            case "luz":
+                if(!$sender instanceof Player) {
+                    $sender->sendMessage("§l§cERROR: §r§a Você deve estar no jogo para executar este comando");
+                    return true;
+                }
+               $effect = new EffectInstance(VanillaEffects::NIGHT_VISION(), 20*99999, 2);
+        if(!$sender->getEffects()->has(VanillaEffects::NIGHT_VISION())){
+            $sender->getEffects()->add($effect);
+            $sender->sendTitle("§bLUZ ATIVA!");
+return true;
+        }else{
+            $sender->getEffects()->remove(VanillaEffects::NIGHT_VISION());
+           $sender->sendTitle("§cLUZ APAGADA");
+return true;
+        }
+}
+}
+}
